b549833baa470c9c2aa13b6ea9c58ed2fb0f02ee
Khang-Truong <khangtruong1995@gmail.com>
2023-02-28 14:12:07 -0800

    Add a new project

---
+++ backend/src/main/java/com/example/backend/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.example.backend.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.example.backend.model.Employee;
+import com.example.backend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // get all employees
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    // create a new employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+}
