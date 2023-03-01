4bf93a9ec0f00f2866c5dd926cdcb6bdfcc43712
Khang-Truong <khangtruong1995@gmail.com>
2023-02-28 22:02:09 -0800

    Create get_employee_by_id REST API

--- backend/src/main/java/com/example/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/example/backend/controller/EmployeeController.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.example.backend.exception.ResourceNotFoundException;
 import com.example.backend.model.Employee;
 import com.example.backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -18,16 +20,22 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     // get all employees
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
 
     // create a new employee rest api
-    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    // get employee base on id, need to return http status
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee=employeeRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException("Employee not exist with id: "+id));
+        return ResponseEntity.ok(employee);
     }
 }
